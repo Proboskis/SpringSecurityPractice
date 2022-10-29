9a9782aa3a1b2811ed22566fe449c195b8a84144
Proboskis <nikolasurla22@gmail.com>
2022-10-29 17:12:41 +0200

    2. Removed WebSecurityConfigurerAdapter because it was deprecated, now using SecurityFilterChain, also using requestMatchers() instead of antMatchers(). Added management api, testing with csrf disabled, using hasAuthority() with role based authentication.

--- src/main/java/com/proboskis/SpringSecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/proboskis/SpringSecurity/security/ApplicationSecurityConfig.java
@@ -1,16 +1,63 @@
 package com.proboskis.SpringSecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static com.proboskis.SpringSecurity.security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
-public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+public class ApplicationSecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/", "index", "/css/*", "/js/*").permitAll().anyRequest().authenticated().and().httpBasic();
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable().authorizeHttpRequests()
+                .requestMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .requestMatchers("/api/**").hasRole(STUDENT.name())
+                .requestMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.name())
+                .requestMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.name())
+                .requestMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.name())
+                .requestMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
+                .anyRequest().authenticated().and().httpBasic();
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() throws Exception {
+        UserDetails annaSmithUser = User.builder().username("anna smith")
+                .password(passwordEncoder.encode("password"))
+                .roles(STUDENT.name()).build(); // ROLE_STUDENT
+
+        UserDetails LindaUser = User.builder().username("Linda")
+                .password(passwordEncoder.encode("password123"))
+                .roles(ADMIN.name()).build();
+
+        UserDetails tomUser = User.builder().username("tom")
+                .password(passwordEncoder.encode("password123"))
+                .roles(ADMINTRAINEE.name()).build();
+
+        return new InMemoryUserDetailsManager(
+                annaSmithUser,
+                LindaUser,
+                tomUser
+        );
     }
 }
